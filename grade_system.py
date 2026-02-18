3b4234b706795a40828dd208674fece817bbd64d
mononokimemory-gif <mononokimemory@gmail.com>
2026-02-18 10:17:28 +0200

    calculate the average

--- grade_system.py
+++ grade_system.py
@@ -1,2 +1,11 @@
-average = sum(marks) / 5
+marks = []
+
+for i in range(1, 6):
+    while True:
+        mark = float(input(f"Enter mark {i} (0 - 100): "))
+        if 0 <= mark <= 100:
+            marks.append(mark)
+            break
+        else:
+            print("❌ Invalid mark! Enter between 0 and 100.")average = sum(marks) / 5
 print("Average:", round(average, 2))
